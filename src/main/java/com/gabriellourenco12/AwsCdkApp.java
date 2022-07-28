16b7dc32705532367e5d11e31e3230f17e0e5bdc
Gabriel Lourenço <gabriel9lourenco@gmail.com>
2022-07-28 00:57:35 -0300

    implementando o rds

--- src/main/java/com/gabriellourenco12/AwsCdkApp.java
+++ src/main/java/com/gabriellourenco12/AwsCdkApp.java
@@ -6,13 +6,17 @@ public class AwsCdkApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        VpcStack VpcStack = new VpcStack(app, "Vpc");
+        VpcStack vpcStack = new VpcStack(app, "Vpc");
 
-        ClusterStack clusterStack = new ClusterStack(app, "Cluster", VpcStack.getVpc());
-        clusterStack.addDependency(VpcStack);
+        ClusterStack clusterStack = new ClusterStack(app, "Cluster", vpcStack.getVpc());
+        clusterStack.addDependency(vpcStack);
+
+        RdsStack rdsStack = new RdsStack(app, "Rds", vpcStack.getVpc());
+        rdsStack.addDependency(vpcStack);
 
         Service01Stack service01Stack = new Service01Stack(app, "Service01", clusterStack.getCluster());
         service01Stack.addDependency(clusterStack);
+        service01Stack.addDependency(rdsStack);
 
         app.synth();
     }
