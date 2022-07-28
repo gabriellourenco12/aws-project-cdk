3ce225c8ee94eb8f04671a3b026f2cc5db9fd08b
Gabriel Lourenço <gabriel9lourenco@gmail.com>
2022-07-28 00:48:14 -0300

    configuracao do rds

--- src/main/java/com/gabriellourenco12/RdsStack.java
+++ src/main/java/com/gabriellourenco12/RdsStack.java
@@ -1,10 +1,11 @@
 package com.gabriellourenco12;
 
-import software.amazon.awscdk.CfnParameter;
-import software.amazon.awscdk.Stack;
-import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.*;
 import software.amazon.awscdk.services.ec2.*;
+import software.amazon.awscdk.services.rds.*;
 import software.constructs.Construct;
+
+import java.util.Collections;
 
 public class RdsStack extends Stack {
     public RdsStack(final Construct scope, final String id, Vpc vpc) {
@@ -21,5 +22,36 @@ public class RdsStack extends Stack {
 
         ISecurityGroup iSecurityGroup = SecurityGroup.fromSecurityGroupId(this, id, vpc.getVpcDefaultSecurityGroup());
         iSecurityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(3306));
+
+        DatabaseInstance databaseInstance = DatabaseInstance.Builder
+                .create(this, "Rds01")
+                .instanceIdentifier("aws-project-db")
+                .engine(DatabaseInstanceEngine.mysql(MySqlInstanceEngineProps.builder()
+                        .version(MysqlEngineVersion.VER_5_7)
+                        .build()))
+                .vpc(vpc)
+                .credentials(Credentials.fromUsername("admin",
+                        CredentialsFromUsernameOptions.builder()
+                                .password(SecretValue.unsafePlainText(databasePass.getValueAsString()))
+                                .build()))
+                .instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO))
+                .multiAz(false)
+                .allocatedStorage(10)
+                .securityGroups(Collections.singletonList(iSecurityGroup))
+                .vpcSubnets(SubnetSelection.builder()
+                        .subnets(vpc.getPrivateSubnets())
+                        .build())
+                .build();
+
+        CfnOutput.Builder.create(this, "rds-endpoint")
+                .exportName("rds-endpoint")
+                .value(databaseInstance.getDbInstanceEndpointAddress())
+                .build();
+
+        CfnOutput.Builder.create(this, "rds-password")
+                .exportName("rds-password")
+                .value(databasePass.getValueAsString())
+                .build();
+
     }
 }
